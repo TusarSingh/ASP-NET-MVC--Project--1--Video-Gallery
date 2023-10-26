8dc83c7db1d3c29a1faa77842185f811c0e0f42b
Tusar Singh <rts0941@gmail.com>
2023-10-26 13:17:48 +0530

    Changing The Model & Use DbContext by Code First Approach

--- Video-Gallery/Models/DBCS.cs
+++ Video-Gallery/Models/DBCS.cs
@@ -3,24 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-
 using System.Data.Entity;
-using Microsoft.AspNet.Identity.EntityFramework;
-
-
 
 namespace Vidly.Models
 {
-    public class DBCS : IdentityDbContext//<ApplicationUser>
+    public class DBCS : DbContext
     {
-        public DBCS()
-        {
-
-        }
-
-        public DbSet<ApplicationUser> AspNetUser { get; set; }
-
+        public DbSet<Customer> Customers { get; set; }
 
     }
-
 }
--- Video-Gallery/ViewModel/RandomMovieViewModel.cs
+++ Video-Gallery/ViewModel/RandomMovieViewModel.cs
@@ -10,7 +10,6 @@ namespace Vidly.ViewModel
 {
     public class RandomMovieViewModel
     {
-
         public Movie Movie { get; set; }
 
         public List<Customer> Customers { get; set; }
