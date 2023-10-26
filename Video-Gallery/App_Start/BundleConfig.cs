46d5c68a4559c7c160e21079bc72c2cc7dbacfab
Tusar Singh <rts0941@gmail.com>
2023-10-26 16:22:51 +0530

    Styling Validation Errors

--- Video-Gallery/App_Start/BundleConfig.cs
+++ Video-Gallery/App_Start/BundleConfig.cs
@@ -19,12 +19,13 @@ namespace Vidly
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+            //bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+            //          "~/Scripts/bootstrap.js",
+            //          "~/Scripts/bootstrap.bundle.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/Site.css"));
         }
     }
 }
